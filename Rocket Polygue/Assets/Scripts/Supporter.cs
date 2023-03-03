a6a82f61a0f3b3a5338f93be189401494b981a77
Mylan <developerspanly@icloud.com>
2023-03-03 19:24:09 +0100

    New Stadium Background and Opti

--- Rocket Polygue/Assets/Scripts/Supporter.cs
+++ Rocket Polygue/Assets/Scripts/Supporter.cs
@@ -9,16 +9,9 @@ public class Supporter : MonoBehaviour
     public Vector3 startPosition;
     public Vector3 endPosition;
     private bool movingUp = true;
-    public List<Material> supporterColor;
 
     void Start()
     {
-        MeshRenderer renderer = GetComponent<MeshRenderer>();
-        if (renderer != null && supporterColor.Count > 0) // Vérifier que le composant MeshRenderer et la liste de matériaux ne sont pas nuls
-        {
-            int randomIndex = Random.Range(0, supporterColor.Count); // Générer un index aléatoire dans la plage des indices de la liste de matériaux
-            renderer.material = supporterColor[randomIndex];
-        }
         random = Random.Range(0.01f,0.1F);
         startPosition = transform.position;
         endPosition = startPosition + new Vector3(0, random, 0);
